eddfe3684912db71483ef76598a3e74df12fe284
Jessica Shi <jessicashi@gmail.com>
2022-02-08 21:58:04 -0500

    span in datapoint

--- include/parcluster/api/datapoint.h
+++ include/parcluster/api/datapoint.h
@@ -11,11 +11,14 @@ namespace research_graph {
 namespace in_memory {
 
 struct DataPoint {
-  std::vector<float> coordinates;
+  absl::Span<const float> coordinates;
+  absl::Span<const uint64_t> indices;
 
   DataPoint() {}
-  explicit DataPoint(const std::vector<float>& coordinates)
+  explicit DataPoint(absl::Span<const float> coordinates)
     : coordinates(coordinates) {}
+  DataPoint(absl::Span<const float> coordinates, absl::Span<uint64_t> indices)
+    : coordinates(coordinates), indices(indices) {}
 };
 
 }  // namespace in_memory
